cb36ae9b7d1a39c441fe3e8b8692aa2ec0aee0d3
maneeshpatel777 <maneesh777@icloud.com>
2024-08-12 23:26:59 +0100

    got the basics of the jquery banner working

--- js/banner.js
+++ js/banner.js
@@ -1,1 +1,10 @@
 console.log("hello from banner.js");
+
+$("#banner").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+});
