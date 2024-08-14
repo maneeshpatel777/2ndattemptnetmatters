d1f35031a52ddd2ed29add2391891aceb6f989f5
maneeshpatel777 <maneesh777@icloud.com>
2024-08-14 10:36:26 +0100

    added hamburger animation, made hamburger icon clickable (issue was due to bubbling and triggering the function twice), sidebar finished

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburgerButton = document.getElementById("header-nav-button");
+  const hamburgerIcon = hamburgerButton.querySelector(".hamburger-icon");
   const sidebar = document.querySelector(".sidebar");
   const mainContent = document.getElementById("main-content");
 
@@ -7,9 +8,15 @@ document.addEventListener("DOMContentLoaded", function () {
     sidebar.classList.toggle("active");
     mainContent.classList.toggle("shifted");
     document.body.classList.toggle("no-scroll");
+    hamburgerIcon.classList.toggle("active");
   }
 
   hamburgerButton.addEventListener("click", toggleSidebar);
+  hamburgerIcon.addEventListener("click", function (event) {
+    // Prevent the event from bubbling up and toggling the sidebar function again in the button (as the hamburger icon is inside the button), thus stopping it from working
+    event.stopPropagation();
+    toggleSidebar();
+  });
 
   document.body.addEventListener("click", function (event) {
     if (this.classList.contains("no-scroll") && !sidebar.contains(event.target) && event.target !== hamburgerButton) {
